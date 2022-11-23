ee3566c8112232c9c4d53e55b284102c4350d503
Hao-q <51706882+Hao-q@users.noreply.github.com>
2022-11-23 12:09:44 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '小白',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'ogz286fBMiZN4r3-TtACwhwn9Bvc',
+      id: 'ogz286dg9sp_8l_yDztyiENGdAj4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'aQsYgwd_9ZXMoB8-1q7_nrlHOizYkmQzY8aiu9QiEDs',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
